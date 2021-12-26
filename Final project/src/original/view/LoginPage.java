24e9856a9eb8fc2c201d53f043c1b958a659fdf8
mahtab-XX <96537717+mahtab-XX@users.noreply.github.com>
2021-12-26 21:40:55 +0330

    Update LoginPage.java

--- Final project/src/original/view/LoginPage.java
+++ Final project/src/original/view/LoginPage.java
@@ -1,11 +1,17 @@
 package original.view;
 
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.SubScene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Pane;
 import javafx.application.Application;
 import javafx.scene.layout.Pane.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 
 public class LoginPage extends TabPane {
@@ -15,32 +21,49 @@ public class LoginPage extends TabPane {
     private Button enter_sighnin;
 
     public LoginPage() {
-        Label username = new Label("Username");
+        Image imagefirstpage = new Image("https://cdn.wallpapersafari.com/69/19/iLajcO.jpg");
+        Label username = new Label("    Username");
+        username.setTextFill(Color.GREEN);
+        username.setFont(Font.font ("FontPosture.ITALIC", 18));
         username.setAlignment(Pos.CENTER);
-        username.setPrefHeight(50);
+        username.setPrefHeight(30);
         entered_Username = new TextField();
-        Label password = new Label("password");
-        password.setPrefHeight(50);
+        Label password = new Label("    password");
+        password.setPrefHeight(30);
+        password.setAlignment(Pos.CENTER);
+        password.setTextFill(Color.BLUE);
+        password.setFont(Font.font ("FontPosture.ITALIC", 18));
         entered_password = new PasswordField();
         enter_login = new Button("login");
+        enter_login.setPrefHeight(30);
+        enter_login.setPrefWidth(240);
         VBox boxlogin = new VBox(username, entered_Username, password, entered_password, enter_login);
         boxlogin.setAlignment(Pos.TOP_LEFT);
-        boxlogin.setPrefWidth(240);
+        boxlogin.setMaxWidth(240);
         boxlogin.setPrefHeight(50);
+        boxlogin.setSpacing(20);
         Tab login = new Tab("longin", boxlogin);
 
         Label username2 = new Label("username");
-        username2.setPrefHeight(50);
-        username.setAlignment(Pos.CENTER);
+        username2.setTextFill(Color.GREEN);
+        username2.setFont(Font.font ("FontPosture.ITALIC", 18));
+        username2.setPrefHeight(30);
+        username2.setAlignment(Pos.CENTER);
         entered_Username = new TextField();
         Label password2 = new Label("password");
-        password2.setPrefHeight(50);
+        password2.setPrefHeight(30);
+        password2.setAlignment(Pos.CENTER);
+        password2.setTextFill(Color.BLUE);
+        password2.setFont(Font.font ("FontPosture.ITALIC", 18));
         entered_password = new PasswordField();
         enter_sighnin = new Button("sighn in");
+        enter_sighnin.setPrefHeight(30);
+        enter_sighnin.setPrefWidth(240);
         VBox boxsighnin = new VBox(username2, entered_Username, password2, entered_password, enter_sighnin);
         boxsighnin.setAlignment(Pos.TOP_LEFT);
-        boxsighnin.setPrefWidth(240);
-        boxsighnin.setPrefHeight(50);
+        boxsighnin.setMaxWidth(240);
+        boxsighnin.setMaxHeight(50);
+        boxsighnin.setSpacing(20);
         Tab Sighnin = new Tab("Sighn in", boxsighnin);
 
         this.getTabs().add(login);
