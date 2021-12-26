14a9cda3c3153a492048fb6c1ced25df1708ca25
mahtab-XX <96537717+mahtab-XX@users.noreply.github.com>
2021-12-26 17:30:23 +0330

    Update LoginPage.java

--- Final project/src/original/view/LoginPage.java
+++ Final project/src/original/view/LoginPage.java
@@ -5,31 +5,46 @@ import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Pane;
 import javafx.application.Application;
+import javafx.scene.layout.Pane.*;
+
 
 public class LoginPage extends TabPane {
     private TextField entered_Username;
     private PasswordField entered_password;
     private Button enter_login;
+    private Button enter_sighnin;
 
     public LoginPage() {
         Label username = new Label("Username");
+        username.setAlignment(Pos.CENTER);
+        username.setPrefHeight(50);
         entered_Username = new TextField();
-        Label password = new Label("Username");
+        Label password = new Label("password");
+        password.setPrefHeight(50);
         entered_password = new PasswordField();
         enter_login = new Button("login");
         VBox boxlogin = new VBox(username, entered_Username, password, entered_password, enter_login);
-
-        Tab Sighnin = new Tab("Sighn in", boxlogin);
+        boxlogin.setAlignment(Pos.TOP_LEFT);
+        boxlogin.setPrefWidth(240);
+        boxlogin.setPrefHeight(50);
+        Tab login = new Tab("longin", boxlogin);
 
         Label username2 = new Label("username");
+        username2.setPrefHeight(50);
+        username.setAlignment(Pos.CENTER);
         entered_Username = new TextField();
-        Label password2 = new Label("Username");
+        Label password2 = new Label("password");
+        password2.setPrefHeight(50);
         entered_password = new PasswordField();
-        enter_login = new Button("login");
-        VBox boxsighnin = new VBox(username, entered_Username, password, entered_password, enter_login);
+        enter_sighnin = new Button("sighn in");
+        VBox boxsighnin = new VBox(username2, entered_Username, password2, entered_password, enter_sighnin);
+        boxsighnin.setAlignment(Pos.TOP_LEFT);
+        boxsighnin.setPrefWidth(240);
+        boxsighnin.setPrefHeight(50);
+        Tab Sighnin = new Tab("Sighn in", boxsighnin);
 
-        Tab Login = new Tab("Login", boxsighnin);
-        this.getChildren().addAll(boxlogin,boxsighnin);
+        this.getTabs().add(login);
+        this.getTabs().add(Sighnin);
+
     }
 }
-
